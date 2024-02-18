38d91f1035fad357469499870aa2e922b0741a26
Arsalan Ahmed <arskhatri2008@gmail.com>
2024-02-18 13:49:38 +0500

    first commit

---
+++ Name_Cases.js
@@ -0,0 +1,4 @@
+var person_Name = "Arsalan Ahmed";
+console.log(person_Name.toUpperCase());
+console.log(person_Name.toLowerCase());
+console.log(person_Name);
---
+++ Personal_Message.js
@@ -0,0 +1,3 @@
+var personName = "Arsalan Ahmed";
+var message = "would you like to learn some Python today?";
+console.log("Hello " + personName + ", " + message);
